76eadc723b4d4a1c5e08716a55441a70cd1ef3c8
masalmon <maelle.salmon@yahoo.se>
2016-05-05 18:07:20 +0200

    adds source of the data

--- R/data_doc.R
+++ R/data_doc.R
@@ -1,11 +1,12 @@
-#' Table of PM2.5 concentrations in 5 cities 2013-2013
+#' Table of PM2.5 concentrations in 5 cities 2013-2016
 #'
-#' Table of PM2.5 concentrations in 5 cities 2013-2013
+#' Table of PM2.5 concentrations in 5 cities 2013-2016: Delhi, Chennai, Kolkata, Hyderabad, Mumbai. Please refer to
+#' original source (U.S. Embassy and Consulates General in India air quality monitoring program) \url{http://newdelhi.usembassy.gov/airqualitydata.html} for licensing details.
 #'
-#' @format A tbl_df (dplyr)
+#' @format A tbl_df (dplyr) with a \code{POSIXct} datetime column, a \code{character} column with the city name and the PM2.5 concentration in microg/m3 as a \code{numeric} column.
 #' @name pm25_india
 #' @docType data
 #' @keywords data
-#' @examples
-#' data(pm25_india)
+#' @examples data(pm25_india)
 #' View(pm25_india)
+NULL
