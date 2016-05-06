e32ebb5057102d932f061bcf587e3ff825e03f08
masalmon <maelle.salmon@yahoo.se>
2016-05-06 11:35:34 +0200

    provides a simpler example

--- R/redo_plot.R
+++ R/redo_plot.R
@@ -1,25 +1,18 @@
 #' Re-do the time series plot for all cities
 #'
 #' @import ggplot2
-#' @import ggiraph
-#' @importFrom dplyr group_by "%>%" summarize
 #'
 #' @return a ggplot graph
 #' @export
 #'
 #' @examples
-#' p <- redo_plot()
-#' ggiraph::ggiraph(code = {print(p)})
+#' redo_plot()
 redo_plot <- function(){
   utils::data("pm25_india", package = "usaqmindia", envir = environment())
-  p <- pm25_india %>%
-    group_by(day = as.Date(datetime), city) %>%
-               summarize(conc = mean(conc, na.rm = TRUE)) %>%
-               ggplot() +
-      geom_point_interactive(aes(day, conc,
-                                 tooltip = paste(round(conc, digits = 2), as.character(day)))) +
+  p <- ggplot(pm25_india) +
+      geom_point(aes(datetime, conc)) +
       facet_grid(city ~ ., scales = "free_y") +
       ggtitle("US Air Quality Monitoring in India")+
-      ylab(expression(paste("Daily average PM2.5 concentration (", mu, "g/",m^3,")")))
+      ylab(expression(paste("PM2.5 concentration (", mu, "g/",m^3,")")))
   p
 }
